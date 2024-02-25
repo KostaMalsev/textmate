c378ae98e598196cc8881e8d2b4b640b66d9e8c1
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2024-02-25 16:21:30 +0200

    Update main2.js

--- main2.js
+++ main2.js
@@ -1,36 +1,91 @@
+import vsctm from 'https://esm.sh/vscode-textmate';
+import oniguruma from 'https://esm.sh/vscode-oniguruma';
 
-import { TextMateGrammarFactory } from "https://esm.sh/vscode-textmate";
+async function getBuffer(url) {
+  
+  return await (await (await fetch(url)).blob()).arrayBuffer();
+  
+}
 
-// Load the JavaScript grammar
-const grammarFactory = new TextMateGrammarFactory();
-const javascriptGrammar = await grammarFactory.loadGrammar("JavaScript.plist");
+// https://unpkg.com/vscode-oniguruma@1.5.1/release/onig.wasm
+const wasmBin = await getBuffer('https://unpkg.com/vscode-oniguruma@1.5.1/release/onig.wasm');
+const vscodeOnigurumaLib = oniguruma.loadWASM(wasmBin).then(() => {
+    return {
+        createOnigScanner(patterns) { return new oniguruma.OnigScanner(patterns); },
+        createOnigString(s) { return new oniguruma.OnigString(s); }
+    };
+});
 
-// Define a function to highlight JavaScript code
-const highlightJavaScript = (code) => {
-  // Tokenize the code
-  const tokens = javascriptGrammar.tokenizeLine(code);
+// Create a registry that can create a grammar from a scope name.
+const registry = new vsctm.Registry({
+    onigLib: vscodeOnigurumaLib,
+    loadGrammar: async (scopeName) => {
+        if (scopeName === 'source.js') {
+            // https://github.com/textmate/javascript.tmbundle/blob/master/Syntaxes/JavaScript.plist
+            
+            const resp = await fetch('https://raw.githubusercontent.com/textmate/javascript.tmbundle/master/Syntaxes/JavaScript.plist');
+            
+            const data = await resp.text();
+            
+            return vsctm.parseRawGrammar(data);
+        }
+        console.log(`Unknown scope name: ${scopeName}`);
+        return null;
+    }
+});
 
-  // Create an HTML string to store the highlighted code
-  let html = "";
+// Load the JavaScript grammar and any other grammars included by it async.
+registry.loadGrammar('source.js').then(grammar => {
+    const text = [
+        `function sayHello(name) {`,
+        `\treturn "Hello, " + name;`,
+        `}`
+    ];
+    let ruleStack = vsctm.INITIAL;
+    for (let i = 0; i < text.length; i++) {
+        const line = text[i];
+        const lineTokens = grammar.tokenizeLine(line, ruleStack);
+        console.log(`\nTokenizing line: ${line}`);
+        for (let j = 0; j < lineTokens.tokens.length; j++) {
+            const token = lineTokens.tokens[j];
+            console.log(` - token from ${token.startIndex} to ${token.endIndex} ` +
+              `(${line.substring(token.startIndex, token.endIndex)}) ` +
+              `with scopes ${token.scopes.join(', ')}`
+            );
+        }
+        ruleStack = lineTokens.ruleStack;
+    }
+});
 
-  // Loop through the tokens and add them to the HTML string
-  for (const token of tokens) {
-    // Get the CSS class for the token
-    const className = javascriptGrammar.tokenToScope(token.scopes[0]);
+/* OUTPUT:
 
-    // Add the token to the HTML string, wrapped in a span with the appropriate CSS class
-    html += `<span class="${className}">${code.substring(token.startIndex, token.endIndex)}</span>`;
-  }
+Unknown scope name: source.js.regexp
 
-  // Return the HTML string
-  return html;
-};
+Tokenizing line: function sayHello(name) {
+ - token from 0 to 8 (function) with scopes source.js, meta.function.js, storage.type.function.js
+ - token from 8 to 9 ( ) with scopes source.js, meta.function.js
+ - token from 9 to 17 (sayHello) with scopes source.js, meta.function.js, entity.name.function.js
+ - token from 17 to 18 (() with scopes source.js, meta.function.js, punctuation.definition.parameters.begin.js
+ - token from 18 to 22 (name) with scopes source.js, meta.function.js, variable.parameter.function.js
+ - token from 22 to 23 ()) with scopes source.js, meta.function.js, punctuation.definition.parameters.end.js
+ - token from 23 to 24 ( ) with scopes source.js
+ - token from 24 to 25 ({) with scopes source.js, punctuation.section.scope.begin.js
 
-// Get the JavaScript code from the user
-const code = prompt("Enter some JavaScript code:");
+Tokenizing line:        return "Hello, " + name;
+ - token from 0 to 1 (  ) with scopes source.js
+ - token from 1 to 7 (return) with scopes source.js, keyword.control.js
+ - token from 7 to 8 ( ) with scopes source.js
+ - token from 8 to 9 (") with scopes source.js, string.quoted.double.js, punctuation.definition.string.begin.js
+ - token from 9 to 16 (Hello, ) with scopes source.js, string.quoted.double.js
+ - token from 16 to 17 (") with scopes source.js, string.quoted.double.js, punctuation.definition.string.end.js
+ - token from 17 to 18 ( ) with scopes source.js
+ - token from 18 to 19 (+) with scopes source.js, keyword.operator.arithmetic.js
+ - token from 19 to 20 ( ) with scopes source.js
+ - token from 20 to 24 (name) with scopes source.js, support.constant.dom.js
+ - token from 24 to 25 (;) with scopes source.js, punctuation.terminator.statement.js
 
-// Highlight the code
-const highlightedCode = highlightJavaScript(code);
+Tokenizing line: }
+ - token from 0 to 1 (}) with scopes source.js, punctuation.section.scope.end.js
 
-// Output the highlighted code to the console
-console.log(highlightedCode);
+*/
+
